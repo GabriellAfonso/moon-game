f6230c46626f5cd96f74b0a88e20fcb1f0178bea
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-06 21:50:12 -0300

    feat: add PlayerSession and PlayerProfileDTO for player management and profile handling

---
+++ Assets/Scripts/Core/Session/PlayerSession.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerSession : MonoBehaviour
+{
+    public static PlayerSession Instance { get; private set; }
+
+    public string Nickname { get; private set; }
+    public string Icon { get; private set; }
+    public int Level { get; private set; }
+    public int Experience_points { get; private set; }
+    public int Coins { get; private set; }
+    public int Credits { get; private set; }
+    
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetProfile(PlayerProfileDTO profile)
+    {
+        Nickname = profile.nickname;
+        Icon = profile.icon;
+        Level = profile.level;
+        Experience_points = profile.experience_points;
+        Coins = profile.coins;
+        Credits = profile.credits;
+
+        Debug.Log(profile.nickname);
+
+    }
+}
---
+++ Assets/Scripts/DTO/PlayerProfileDTO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerProfileDTO
+{
+    public string nickname;
+    public string icon;
+    public int level;
+    public int experience_points;
+    public int coins;
+    public int credits;
+}
